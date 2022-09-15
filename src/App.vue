e6efa6bbd5ae46fed45f74d51b4b4b16a44616cc
RodrJavi <nb2javi@gmail.com>
2022-09-14 23:31:51 -0500

    Made tip percent component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+  import {ref} from 'vue';
+  import TipPercent from './components/TipPercent.vue';
+
+  const cost = ref();
+  const guests = ref();
+
+</script>
 
 <template>
   <div className="bg-lgcyan">
@@ -6,31 +13,9 @@
     <div className="flex flex-col bg-white rounded-t-xl p-5 gap-5">
       <div className="flex flex-col">
         <label className="text-dgcyan" for="bill">Bill</label>
-        <input name="bill" type="number" className="bg-vlgcyan text-dcyan" />
+        <input v-model="cost" name="bill" type="number" className="bg-vlgcyan text-dcyan" />
       </div>
-      <div>
-        <span className="text-dgcyan">Select Tip %</span>
-        <div className="grid grid-cols-2 gap-3 md:grid-cols-3">
-          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
-            5%
-          </button>
-          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
-            10%
-          </button>
-          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
-            15%
-          </button>
-          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
-            25%
-          </button>
-          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
-            50%
-          </button>
-          <button className="text-gcyan bg-vlgcyan rounded p-1.5 hover:bg-cyan">
-            Custom
-          </button>
-        </div>
-      </div>
+      <TipPercent/>
       <div>
         <label for="people" className="text-dgcyan">Number of People</label>
         <input
---
+++ src/components/TipPercent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue';
+
+const percentage = ref();
+const emit = defineEmits(['percent'])
+
+emit('percent')
+</script>
+
+<template>
+    <div>
+        <span className="text-dgcyan">Select Tip %</span>
+        <form className="grid grid-cols-2 gap-3 md:grid-cols-3">
+          <input type="button" value="5%" className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+          <input type="button" value="10%" className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+          <input type="button" value="15%" className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+          <input type="button" value="25%" className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+          <input type="button" value="50%" className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+          <input type="text" value="Custom" className="text-gcyan bg-vlgcyan rounded p-1.5 hover:bg-cyan">
+        </form>
+    </div>
+</template>
