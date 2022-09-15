8be65df161811795083d8990da5545b9630de70f
Javier Rodriguez <nb2javi@gmail.com>
2022-09-14 22:35:36 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+  <div className="bg-lgcyan">
+    <h2 className="text-dgcyan">Splitter</h2>
+    <div className="flex flex-col bg-white rounded-t-xl p-5 gap-5">
+      <div className="flex flex-col">
+        <label className="text-dgcyan" for="bill">Bill</label>
+        <input name="bill" type="number" className="bg-vlgcyan text-dcyan" />
+      </div>
+      <div>
+        <span className="text-dgcyan">Select Tip %</span>
+        <div className="grid grid-cols-2 gap-3 md:grid-cols-3">
+          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+            5%
+          </button>
+          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+            10%
+          </button>
+          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+            15%
+          </button>
+          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+            25%
+          </button>
+          <button className="text-white bg-dcyan rounded p-1.5 hover:bg-cyan">
+            50%
+          </button>
+          <button className="text-gcyan bg-vlgcyan rounded p-1.5 hover:bg-cyan">
+            Custom
+          </button>
+        </div>
+      </div>
+      <div>
+        <label for="people" className="text-dgcyan">Number of People</label>
+        <input
+          type="number"
+          name="people"
+          id=""
+          className="bg-vlgcyan text-dcyan"
+        />
+      </div>
+      <div className="flex flex-col bg-dcyan p-5">
+        <span className="text-white">Tip Amount/person</span>
+        <span className="text-white">Total/person</span>
+        <button className="bg-cyan hover:bg-lgcyan">RESET</button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      md: "376px",
+      // => @media (min-width: 576px) { ... }
+    },
+    colors: {
+      cyan: "hsl(172 67% 45%)",
+      dcyan: "hsl(183, 100%, 15%)",
+      dgcyan: "hsl(186, 14%, 43%)",
+      gcyan: "hsl(184, 14%, 56%)",
+      lgcyan: "hsl(185, 41%, 84%)",
+      vlgcyan: "hsl(189, 41%, 97%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
